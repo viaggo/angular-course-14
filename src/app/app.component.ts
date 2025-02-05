e1244ded9effd0e5eff629b07487cf0f981c0b2c
isabel jim her <ijhviaggo@gmail.com>
2025-02-04 21:07:54 -0600

    objetos json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   title = 'curso14_16';
 
   constructor(){
+   // https://www.youtube.com/watch?v=PMZaF18pkHg&list=PLkmDVMKx2R1lk_regbIa_6k8TqBOxauit&index=3
+
     const auxmap =  [1,2,3,4,5,6].map( n => {
       return n * 2;
     })
@@ -39,7 +41,103 @@ export class AppComponent {
     console.log("join " + auxJoin);
 
     const auxSplit ="La vida es importante".split(' ');
-    console.log("posicion 1 " + auxSplit[1] )
+    console.log("posicion 1 " + auxSplit[1] );
+
+    /***********************************************************************/
+    //OPERADOR SPLICE
+    const auxSplic = [1,2,3,4,5,6,7,8,9].splice(1,4);
+    console.log(auxSplic);  //obtiene los elementos 
+
+   
+    //elimina el elemento de un array
+    //devuelve lo eliminado y modifica el array original
+    const auxSplice = [1,2,3,4,5,6,7,8,9].splice(2,2);
+    console.log("elementos splice" + auxSplice);
+
+    //reduce
+ 
+    const auxReduce = [1,2,3,4].reduce((pre,value,i) => {
+      return pre + value;
+    }, 0)
+    console.log(auxReduce);
+
+    //https://www.youtube.com/watch?v=PMZaF18pkHg&list=PLkmDVMKx2R1lk_regbIa_6k8TqBOxauit&index=5
+    //min 12
+
+    const auxJson = {
+      id: 1,
+      name : 'Juan',
+      lastname:'Perez'
+    };
+
+    const auxEntries = Object.entries(auxJson);
+    console.log("auxEntries: " + auxEntries);
+
+    const aunkeys = Object.keys(auxJson);
+    console.log("aunkesy: " + aunkeys);
+
+    const aunValues = Object.values (auxJson);
+    console.log("aunJson: " + aunValues);
+
+    //typescript
+    let miVar : number = 20;
+    let miDato : String = 'Este es mi información';
+
+    for (let i = 1; i <= 5; i++) {
+      console.log('index : ' + i);
+    }
+
+    //spread operator, Rest oprator
+    //spread operator  (...)
+    let a = [1,2,3,4,5];
+    let b = [...a, 6,7,6,8];
+
+    console.log("Spread operator: a " + a);
+    console.log("Spread operator: a.. + b " + b);
+
+    this.getRest('uno','dos','tres')
+
+
+    //destructuracion
+    const person = {
+      id : 1,
+      name : 'Juan',
+      country : {
+        idCountry : 1,
+        cod : 'LP'
+      }
+    }
+
+    const {country, id} = person;
+    console.log('country: ' + country);
+
+
+    //ejercicio
+
+    const data = {
+      0: { id:1, name : 'Pedro', age: 20 },
+      1: { id:2, name : 'Julio', age: 25 }
+    }
+
+    //transforma a un array de objeto
+    let arrayInfo = Object.values(data);
+
+    arrayInfo.reduce((prev,value) => {
+      if (value.age> 20) {
+        return prev + value.age;
+      }
+      return prev;
+    }, 0 );
+
+    let mayores = arrayInfo.filter(p => p.age > 20).map(p => p.id)  ;
+    console.log(mayores);
+    
+  }
+
+  getRest(...param: string[]) {
+    for(let d in param) {
+      console.log('valores: ' + d);
+    }
   }
 
   getName(name: string) : string {
